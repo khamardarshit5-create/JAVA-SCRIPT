84dccf843b6fa274f223255e7a0c4038d7a5c8fe
khamardarshit5-create <khamardarshit5@gmail.com>
2026-02-16 02:59:55 +0000

    veriable

---
+++ day-1ofjs/veriable.js
@@ -0,0 +1,13 @@
+const accountId =114523
+let accountemail = "khamardarshit5@gmail.com"
+var accountpassword ="15213"
+accountcity = "surat"
+
+// accountId =123  //not allowed 
+
+accountemail ="bsda@gmail,com"
+accountpassword ="654654"
+accountcity ="aanand"
+
+console.log(accountId);
+console.table([accountId, accountemail, accountpassword, accountcity])
