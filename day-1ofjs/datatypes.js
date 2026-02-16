d712262d25dd13c282ac8b10719661e2f28d2e9f
khamardarshit5-create <khamardarshit5@gmail.com>
2026-02-16 03:31:29 +0000

    document

--- day-1ofjs/datatypes.js
+++ day-1ofjs/datatypes.js
@@ -1,5 +1,9 @@
 "use strict"; //treat all js code as newer version 
 
 // alert(3+3)  // we are use nodejs not browser
+/* 
+    documantesion of it on tc39.es ECMA script or mdn
+*/ 
+
 
 console.log(3+3)
