b2ee1e6a38c4d17e4654d6f8e96c60f252824923
khamardarshit5-create <khamardarshit5@gmail.com>
2026-02-16 03:05:31 +0000

    done

--- day-1ofjs/veriable.js
+++ day-1ofjs/veriable.js
@@ -3,6 +3,8 @@ let accountemail = "khamardarshit5@gmail.com"
 var accountpassword ="15213"
 accountcity = "surat"
 
+let accountstate
+
 // accountId =123  //not allowed 
 
 accountemail ="bsda@gmail,com"
@@ -10,4 +12,10 @@ accountpassword ="654654"
 accountcity ="aanand"
 
 console.log(accountId);
-console.table([accountId, accountemail, accountpassword, accountcity])
+console.table([accountId, accountemail, accountpassword, accountcity, accountstate])
+
+/*
+
+Prefer not to use var
+because of  issue in block scope and functional scope
+*/
