b4d9f18d1d3b6b8bcf7ae362eb33e35601bd892b
khamardarshit5-create <khamardarshit5@gmail.com>
2026-02-16 03:39:29 +0000

    datatype.done

--- day-1ofjs/datatypes.js
+++ day-1ofjs/datatypes.js
@@ -7,3 +7,24 @@
 
 
 console.log(3+3)
+
+let name ="darshit"
+let age =24
+let isloggedin =false
+
+// number =>2 to power 53
+// bigint
+// string=>""
+// boolean =>true/false
+// null => standaloan value
+// undefined =>
+// symbol =>unique
+
+// object
+
+console.log(typeof"darshit");
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+
